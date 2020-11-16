458b398204ec2b44fdc43c506a95112476e6fd39
Radu Patulescu <radu.patulescu@gmail.com>
2020-11-16 10:56:07 -0800

    Add API endpoint for session details

--- journal/urls.py
+++ journal/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
@@ -7,5 +8,8 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:session_id>/rate', views.rate, name='rate_session'),
-    path('api_sessions', views.session_list, name='api_sessions')
+    path('api_sessions', views.session_list, name='api_sessions_list'),
+    path('api_sessions/<int:pk>', views.session_details, name='api_sessions_details')
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- journal/views.py
+++ journal/views.py
@@ -13,14 +13,14 @@ from .serializers import SessionSerializer, ClimbSerializer, GradeSerializer
 from .models import Session
 
 @api_view(['GET', 'POST'])
-def session_list(request):
+def session_list(request, format=None):
     """
     API endpoint that allows sessions to be viewed or edited.
     """
     if request.method == 'GET':
         queryset = Session.objects.all().order_by('-date')
         serializer_class = SessionSerializer(queryset, many=True)
-        return JsonResponse(serializer_class.data, safe=False)
+        return Response(serializer_class.data)
 
     elif request.method == 'POST':
         serializer = SessionSerializer(data=request.data)
@@ -28,6 +28,17 @@ def session_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def session_details(request, pk, format=None):
+    try:
+        session = Session.objects.get(pk=pk)
+    except Session.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = SessionSerializer(session)
+        return Response(serializer.data)
 
 # Create your views here.
 class IndexView(generic.ListView):
