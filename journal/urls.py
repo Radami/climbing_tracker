29259df7d964323a4c3f242507b20ea81013549d
Radu Patulescu <radu.patulescu@gmail.com>
2020-11-18 00:51:25 -0800

    Add class based views for API endpoints

--- journal/urls.py
+++ journal/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:session_id>/rate', views.rate, name='rate_session'),
-    path('api_sessions', views.session_list, name='api_sessions_list'),
-    path('api_sessions/<int:pk>', views.session_details, name='api_sessions_details')
+    path('api', views.SessionList.as_view()),
+    path('api/<int:pk>', views.SessionDetails.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- journal/views.py
+++ journal/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse, Http404
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
 from django.urls import reverse
@@ -8,37 +8,83 @@ from rest_framework import status
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .serializers import SessionSerializer, ClimbSerializer, GradeSerializer
 from .models import Session
 
-@api_view(['GET', 'POST'])
-def session_list(request, format=None):
+class SessionList(APIView):
     """
-    API endpoint that allows sessions to be viewed or edited.
+    List all climbing sessions or create a new one
     """
-    if request.method == 'GET':
-        queryset = Session.objects.all().order_by('-date')
-        serializer_class = SessionSerializer(queryset, many=True)
-        return Response(serializer_class.data)
+    def get(self, request, format=None):
+        sessions = Session.objects.all().order_by('-date')
+        serializer = SessionSerializer(sessions, many=True)
+        return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = SessionSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
-def session_details(request, pk, format=None):
-    try:
-        session = Session.objects.get(pk=pk)
-    except Session.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'GET':
+class SessionDetails(APIView):
+    """
+    Retrieve, update or delete a session
+    """
+    def get_object(self, pk):
+        try:
+            return Session.objects.get(pk=pk)
+        except Session.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        session = self.get_object(pk)
         serializer = SessionSerializer(session)
         return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        session = self.get_object(pk)
+        serializer = SessionSerializer(session, data=request.data)
+        if serializer.is_valid():
+            session.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        session = self.get_object(pk)
+        session.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# @api_view(['GET', 'POST'])
+# def session_list(request, format=None):
+#     """
+#     API endpoint that allows sessions to be viewed or edited.
+#     """
+#     if request.method == 'GET':
+#         queryset = Session.objects.all().order_by('-date')
+#         serializer_class = SessionSerializer(queryset, many=True)
+#         return Response(serializer_class.data)
+
+#     elif request.method == 'POST':
+#         serializer = SessionSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['GET'])
+# def session_details(request, pk, format=None):
+#     try:
+#         session = Session.objects.get(pk=pk)
+#     except Session.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+    
+#     if request.method == 'GET':
+#         serializer = SessionSerializer(session)
+#         return Response(serializer.data)
 
 # Create your views here.
 class IndexView(generic.ListView):
