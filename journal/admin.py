e9f11ee8f0b00ee4eb216b7907ecac8855513baf
Radu Patulescu <radu.patulescu@gmail.com>
2023-07-05 07:56:16 +0900

    Refactor Location as separate Model

--- journal/admin.py
+++ journal/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from .models import Grade, Climb, Session, SessionPartner
+from .models import Grade, Climb, Session, SessionPartner, Location
 
 # Register your models here.
 admin.site.register(Grade)
 admin.site.register(Climb)
 admin.site.register(SessionPartner)
+admin.site.register(Location)
 
 class ClimbInline(admin.TabularInline):
     model = Climb
@@ -15,16 +16,20 @@ class SessionPartnerInline(admin.TabularInline):
     model = SessionPartner
     extra = 0
 
+class LocationInLine(admin.TabularInline):
+    model = Location
+    extra = 1
+
 class SessionAdmin(admin.ModelAdmin):
     fieldsets = [
-        ('Date information', {'fields' : ['date', 'center', 'owner']}),
+        ('Date information', {'fields' : ['date', 'location', 'owner']}),
         ('Session information', {'fields' : ['rating']})
     ]
     inlines = [ClimbInline, SessionPartnerInline]
 
-    list_display = ('date', 'rating', 'center')
+    list_display = ('date', 'rating', 'location')
     
-    list_filter = ['date', 'center']
+    list_filter = ['date', 'location']
 
 admin.site.register(Session, SessionAdmin)
 
--- journal/models.py
+++ journal/models.py
@@ -10,9 +10,15 @@ class Grade(models.Model):
     def __str__(self):
         return self.label
 
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey('auth.User', related_name='user_location', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 class Session(models.Model):
-    center = models.CharField(max_length=50)
+    location = models.ForeignKey(Location, related_name='location', on_delete=models.CASCADE)
     date = models.DateTimeField('date of climb')
     rating = models.IntegerField(default=0)
     owner = models.ForeignKey('auth.User', related_name='user_sessions',on_delete=models.CASCADE)
@@ -22,7 +28,7 @@ class Session(models.Model):
         return now - datetime.timedelta(days=5) <= self.date <= now
 
     def __str__(self):
-        return self.center + " on " + str(self.date.date())
+        return self.location.name + " on " + str(self.date.date())
 
 class SessionPartner(models.Model):
     session = models.ForeignKey(Session, related_name="partners", on_delete=models.CASCADE)
@@ -39,3 +45,5 @@ class Climb(models.Model):
 
     def __str__(self):
         return str(self.grade) + " - " + self.comments + " : " + str(self.rating)
+    
+
